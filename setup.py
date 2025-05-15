a21c46d40987c60968c93ff2436fc9ab440651a5
Alessandro <88981047+fone-almosca@users.noreply.github.com>
2025-05-15 12:15:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("./requirements.txt", "r") as f:
 
 setup(
     name="clerk-sdk",
-    version="0.0.1",
+    version="0.1.0",
     description="Library for interacting with Clerk",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
