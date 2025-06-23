85007b6b06a09aefd98de524eed1bf1442dbd134
Alessandro Mosca <a.mosca@f-one.group>
2025-06-23 16:30:33 +0300

    fix: handle case where no coordinates are found in get_coordinates method

--- clerk/gui_automation/client.py
+++ clerk/gui_automation/client.py
@@ -38,7 +38,10 @@ class RPAClerk(BaseClerk):
     def get_coordinates(self, payload: Dict) -> Coords:
         endpoint = "/action_model/get_coordinates"
         res = self.post_request(endpoint=endpoint, json=payload)
-        return Coords(**res.data[0])
+        if res.status_code == 200:
+            if res.data[0] is None:
+                raise RuntimeError("No coordinates found in the response.")
+            return Coords(**res.data[0])
 
 
 class GUIVisionClerk(BaseClerk):
