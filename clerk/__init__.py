f8f9e1ca31c4a2190e97bf39d290e272e9dbd102
Alessandro <a.mosca@f-one.group>
2025-11-24 13:02:36 +0300

    bump version 0.4.14

--- clerk/__init__.py
+++ clerk/__init__.py
@@ -1,4 +1,4 @@
 from .client import Clerk
 
 
-__version__ = "0.4.13"
+__version__ = "0.4.14"
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ gui_requirements = get_requirements("./clerk/gui_automation")
 
 setup(
     name="clerk-sdk",
-    version="0.4.13",
+    version="0.4.14",
     description="Library for interacting with Clerk",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
