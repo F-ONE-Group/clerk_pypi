3396ed9ee7668cb40cc9cd9758f53dabb1556898
Alessandro <a.mosca@f-one.group>
2026-03-19 11:37:13 +0300

    bump version to 1.0.5 and add new action types: wait_for, open_app, force_close_app, maximize_window, minimize_window, close_window, activate_window, save_files, delete_files, get_file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ gui_requirements = get_requirements("./clerk/gui_automation")
 
 setup(
     name="clerk-sdk",
-    version="1.0.4",
+    version="1.0.5",
     description="Library for interacting with Clerk",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
